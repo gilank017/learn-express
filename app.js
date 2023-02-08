3a1521f6c3592292f4226c1624bc79fa0959581e
gilank017 <gilank.zone@gmail.com>
2023-02-09 00:40:27 +0700

    add handle respose route error

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(bodyParser.urlencoded({
 app.use("/", require("./routes/public"))
 
 
+//handle route error
+app.use(function (req, res, next) {
+  res.status(404).json({status: false, message: "Sorry, We don't know the endpoint target"})
+})
+
+
 app.listen(3000, () => {
   console.log("server started on port 3000") 
 })
